ff06644853771a947b70aae473dd8d3c6f4bb6c9
Max922991 <73070516+Max922991@users.noreply.github.com>
2022-03-29 01:13:03 +0300

    Update Task1.java

--- Test1/Task1.java
+++ Test1/Task1.java
@@ -3,6 +3,6 @@ package Test1;
 public class Task1 {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!2");
+        System.out.println("Hello world!223232");
     }
 }
